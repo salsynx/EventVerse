e755e5e609f56e501771336f79e32d5e89a463b5
Salmann <108986052+salsynx@users.noreply.github.com>
2025-10-16 20:48:31 +0530

    Update auth.js

--- auth/auth.js
+++ auth/auth.js
@@ -1,1 +1,15 @@
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyCzBInzu6CYO7SyMFWjw7YHhKDCmxeJ688",
+    authDomain: "eventx-tm-hackattack.firebaseapp.com",
+    projectId: "eventx-tm-hackattack",
+    storageBucket: "eventx-tm-hackattack.appspot.com",
+    messagingSenderId: "128226433491",
+    appId: "1:128226433491:web:089d250300b3988cdeb726"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
 
